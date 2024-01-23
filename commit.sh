1f38bd9f815ee32ff8a3681cf15688bbadec7081
Enes <enes.barut@gmx.com>
2024-01-23 16:45:27 +0100

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+# Fonction pour effectuer le commit et le push
+perform_git_operations() {
+    local commit_message=$1
+
+    # Ajoute tous les fichiers modifiés à l'index Git
+    git add -A
+
+    # Effectue le commit avec le message fourni
+    git commit -m "$commit_message"
+
+    # Pousse les modifications vers le dépôt distant
+    git push
+
+    echo "Les modifications ont été poussées avec succès."
+}
+
 # Demande à l'utilisateur d'entrer le message de commit
 read -p "Entrez le message de commit: " commit_message
 
-# Ajoute tous les fichiers modifiés à l'index Git
-git add -A
-
-# Effectue le commit avec le message fourni
-git commit -m "$commit_message"
-
-# Pousse les modifications vers le dépôt distant
-git push
-
-echo "Les modifications ont été poussées avec succès."
+# Appelle la fonction avec le message de commit
+perform_git_operations "$commit_message"
